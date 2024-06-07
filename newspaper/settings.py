26a0b84967bf8623dceb5993a814661cec44c13a
Victorspec <generalspectec@gmail.com>
2024-06-07 03:47:32 +0100

    third commit

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -134,7 +134,13 @@ DATABASES = {
         
 
 
-
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:1232478847@localhost:5432/emma',
+        conn_max_age=600
+    )
+}
 
 
 
